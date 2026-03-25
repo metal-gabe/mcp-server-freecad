4810dba02129814dbc39b1ddca50aadbcc3332a9
Metal Gabe <gflujan@gmail.com>
2026-03-25 02:53:46 -0700

    feat(refactor): temporarily ignore errors;

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from .server import MCPServerFreeCAD
+from server import MCPServerFreeCAD  # type: ignore[attr-defined]
 
 if __name__ == "__main__":
-    MCPServerFreeCAD().server.run()
+    MCPServerFreeCAD().server.run()  # pyright: ignore[reportUnknownMemberType]
